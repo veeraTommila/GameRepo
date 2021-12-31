7f91d17969405c70387776a931bb8ef05add39c4
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-31 13:07:46 +0200

    My first Unity project
    You need the version 2019.1.6f1 to make this work properly.

---
+++ Naruko_for_elder_unity_all_animations/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)){
+            anim.SetBool("isWalking", true);
+        } else {
+            anim.SetBool("isWalking", false);
+        }
+
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Speed_jump");
+        }
+    }
+}
