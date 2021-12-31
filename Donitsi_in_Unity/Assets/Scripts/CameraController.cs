2a789456f118f116e4ec0b7701704a83c13074e3
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-31 14:00:01 +0200

    My second Unity project
    You need the version 2019.3.6f1 to make this work properly.

---
+++ Donitsi_in_Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Aseta pelinappula kameran kohteeksi.
+    public Transform target;
+
+    public Vector3 offset;  //Kuinka kaukaa kamera seuraa pelinappulaa.
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public float maxKatseluKulma;
+    public float minKatseluKulma;
+
+    public Transform pivot;
+
+    public bool invertY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;  //Kuinka kaukaa kamera seuraa pelinappulaa.                     
+        }
+        //Pivot seuraa kohdetta (pelinappulaa), kun peli on käynnissä.
+        pivot.transform.position = target.transform.position;
+        //pivot.transform.parent = target.transform;
+        pivot.transform.parent = null;
+
+        //Piilotetaan hiiren osoitin, kun peli on käynnissä.
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        pivot.transform.position = target.transform.position;   //Pivot-piste liikkuu pelinappulan kanssa.
+
+        //Pelinappula ei liiku, kun kameraa liikutetaan. Silloin target.Rotate(0, horizontal, 0); onkin pivot.Rotate(0, horizontal, 0);
+
+
+        //Hae hiiren X-sijainti ja käännä kohdetta.
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivot.Rotate(0, horizontal, 0);
+
+        //Hae hiiren Y-sijainti ja käännä pivot-pistettä.
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //pivot.Rotate(-vertical,0 ,0 );
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        } else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        /*
+         * Kameran ylös- ja alas kääntymisen rajojen määrittäminen.
+         * Eulerin kulmat ovat kolme kulmaa, jotka Leonhard Euler esitteli kuvaamaan jäykän rungon suuntaa kiinteään koordinaatistoon nähden.
+         * Nyt kamera ei nytkähtele yllättävästi, kun katsomme pelinappulaa suoraan ylhäältä ja maantasosta.
+         */
+        if (pivot.rotation.eulerAngles.x > maxKatseluKulma && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxKatseluKulma, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minKatseluKulma)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minKatseluKulma, 0, 0);
+        }
+
+        //Move the camera based on the current rotation of the target & the original offset.
+        float desiredYAngle = pivot.eulerAngles.y; //Ennen oli target.eulerAngles.y
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;  //Kuinka kaukaa kamera seuraa pelinappulaa.
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y -.5f, transform.position.z);
+        }
+
+        transform.LookAt(target.transform); //Aseta pelinappula kameran kohteeksi.
+    }
+}
---
+++ Donitsi_in_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;   //Tätä muuttujaa moveSpeed voidaan editoida Unityssä, private floatia ei voida.
+    //public Rigidbody theRB;   //Otetaan Rigidbody käyttöön.
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+
+    public GameObject playerModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //theRB = GetComponent<Rigidbody>();  //Ei tarvitse raahata Unityn play-moodia varten.
+        controller = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         Move around. Set speed of axes X, Y and Z.
+        //X = Horizontal (vasemmalle ja oikealle), Z = Vertical (eteen ja taakse) sekä Y = ylös ja alas.
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        //Onko Unityssä hypylle tarkoitettua näppäintä painettu.
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Muutetaan Y-akselin arvoa.
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }*/
+
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        //Pelinappulan liikkeen kontrollointi on helpompaa.
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+
+        //Onko Unityssä hypylle tarkoitettua näppäintä painettu.
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                //Muutetaan Y-akselin arvoa.
+                moveDirection.y = jumpForce;
+            }
+        }
+        
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * Time.deltaTime * gravityScale);
+
+        //Sovelletaan moveDirectionia Character Controlleriin.
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Pelinappula kääntyy kameran katsoman suunnan mukaan.
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        anim.SetBool("isGrounded", controller.isGrounded);
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
+    }
+}
